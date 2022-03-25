552562d46b2349b126bcbd447559addc902acb90
Saraiv <a15326@alunos.ipca.pt>
2022-03-25 20:53:52 +0000

    Restart a funcionar, mas não passa de nivel

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Sokoban
 {
@@ -10,13 +11,22 @@ namespace Sokoban
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        string levelPath = "../../../Content/SokobanLevels/level1.txt";
+        string[] levelsPath = {"../../../Content/SokobanLevels/level1.txt",
+                                "../../../Content/SokobanLevels/level2.txt",
+                                "../../../Content/SokobanLevels/level3.txt",
+                                "../../../Content/SokobanLevels/level4.txt",
+                                "../../../Content/SokobanLevels/level5.txt" };
+        int currentLevel = 0;
         char[,] map;
+        List<Vector2> objectivePoints;
         const int tileSize = 64;
         int width, height;
-        Vector2 playerPos;
+        Vector2 playerPos, spawnPos;
         KeyboardManager km = new KeyboardManager();
         Texture2D playerTexture, wallTexture, groundTexture, endTexture, crateTexture;
+
+        delegate bool Verification();
+        event Verification OnObjetiveReach;
 
         public Game1()
         {
@@ -28,7 +38,6 @@ namespace Sokoban
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            LoadLevel();
             base.Initialize();
         }
 
@@ -41,6 +50,9 @@ namespace Sokoban
             groundTexture = Content.Load<Texture2D>("GroundGravel_Grass");
             crateTexture = Content.Load<Texture2D>("Crate_Beige");
             endTexture = Content.Load<Texture2D>("EndPoint_Black");
+
+            LoadLevel();
+            OnObjetiveReach += isLevelFinished;
             // TODO: use this.Content to load your game content here
         }
 
@@ -95,7 +107,12 @@ namespace Sokoban
 
         void LoadLevel()
         {
-            string[] lines = File.ReadAllLines(levelPath);
+            if (currentLevel > levelsPath.Length) return;
+
+            objectivePoints = new List<Vector2>();
+            objectivePoints.Clear();
+
+            string[] lines = File.ReadAllLines(levelsPath[currentLevel++]);
             map = new char[lines[0].Length, lines.Length];
             
             for(int x = 0; x < lines[0].Length; x++)
@@ -104,8 +121,16 @@ namespace Sokoban
                 {
                     string currentLine = lines[y];
                     map[x, y] = currentLine[x];
+                    if (currentLine[x] == '.')
+                    {
+                        objectivePoints.Add(new Vector2(x, y));
+                    }
+
                     if (currentLine[x] == 'i')
+                    {
+                        spawnPos = new Vector2(x, y);
                         playerPos = new Vector2(x, y);
+                    }
                 }
             }
 
@@ -143,19 +168,36 @@ namespace Sokoban
                 newPos += Vector2.UnitX;
                 dir = Vector2.UnitX;
             }
+            if (km.IsKeyPressed(Keys.R))
+            {
+                newPos = spawnPos;
+                currentLevel--;
+                LoadLevel();
+            }
 
-            if(map[(int)newPos.X, (int)newPos.Y] == 'X')
+            if (map[(int)newPos.X, (int)newPos.Y] == 'X')
             {
                 newPos = playerPos;
             }
             else if(map[(int)newPos.X, (int)newPos.Y] == 'B')
             {
-                if(map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == ' ' || map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == '.')
+                if(map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == ' ' || 
+                    map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == '.')
                 {
+                    bool isObjective = map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == '.';
                     map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] = 'B';
                     map[(int)(newPos.X), (int)(newPos.Y)] = ' ';
+
+                    foreach (Vector2 pos in objectivePoints)
+                    {
+                        if (pos.X == newPos.X + dir.X && pos.Y == newPos.Y + dir.Y)
+                        {
+                            OnObjetiveReach.Invoke();
+                        }
+                    }
                 }
-                else
+                else if(map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == 'X' ||
+                    map[(int)(newPos.X + dir.X), (int)(newPos.Y + dir.Y)] == 'B')
                 {
                     newPos = playerPos;
                 }
@@ -164,6 +206,19 @@ namespace Sokoban
             map[(int)playerPos.X, (int)playerPos.Y] = ' ';
             playerPos = newPos;
             map[(int)playerPos.X, (int)playerPos.Y] = 'i';
+        }
+
+        bool isLevelFinished()
+        {
+            foreach (Vector2 pos in objectivePoints)
+            {
+                if(map[(int)pos.X, (int)pos.Y] != 'B')
+                {
+                    return false;
+                }
+            }
+            LoadLevel();
+            return true;
         }
     }
 }
